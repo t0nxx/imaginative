08f99f91285496784ae3b93fd54452ea076c92de
t0nxx <mahmoudko1500@hotmail.com>
2021-06-11 17:14:33 +0200

    add seed data

---
+++ prisma/seed-data/users.ts
@@ -0,0 +1,57 @@
+export const users = [
+  {
+    id: 1,
+    name: 'admin account',
+    email: 'a@a.com',
+    photoUrl:
+      'https://gravatar.com/avatar/14978c9024af3c17cf195dff095d79e8?s=400&d=robohash&r=x',
+    featuredProductName: null,
+    featuredProductId: null,
+    // this pass is asd123456
+    password: '$2a$10$8oopKOygF6kzIu9TT47JrehCw9DK8JUW8bBU0DrHbrAtLQPS3eGCC',
+    lang: 'en',
+    createdAt: '2021-06-03 22:07:09.763',
+    updatedAt: '2021-06-06 22:29:19.536',
+    notificationsEnabled: true,
+    provider: 'local',
+    type: 'individual',
+    role: 'admin',
+  },
+  {
+    id: 2,
+    name: 'company account',
+    email: 'b@b.com',
+    photoUrl:
+      'https://gravatar.com/avatar/14978c9024af3c17cf195dff095d79e8?s=400&d=robohash&r=x',
+    featuredProductName: null,
+    featuredProductId: null,
+    // this pass is asd123456
+    password: '$2a$10$8oopKOygF6kzIu9TT47JrehCw9DK8JUW8bBU0DrHbrAtLQPS3eGCC',
+    lang: 'en',
+    createdAt: '2021-06-07 21:00:02.099',
+    updatedAt: '2021-06-07 21:00:02.1',
+    notificationsEnabled: true,
+    provider: 'local',
+    type: 'company',
+    role: 'user',
+  },
+  {
+    id: 3,
+    name: 'institute account',
+    email: 'c@c.com',
+    photoUrl:
+      'https://gravatar.com/avatar/14978c9024af3c17cf195dff095d79e8?s=400&d=robohash&r=x',
+    featuredProductName: null,
+    featuredProductId: null,
+    // this pass is asd123456
+
+    password: '$2a$10$8oopKOygF6kzIu9TT47JrehCw9DK8JUW8bBU0DrHbrAtLQPS3eGCC',
+    lang: 'en',
+    createdAt: '2021-06-07 21:00:02.099',
+    updatedAt: '2021-06-07 21:00:02.1',
+    notificationsEnabled: true,
+    provider: 'local',
+    type: 'institute',
+    role: 'user',
+  },
+];
