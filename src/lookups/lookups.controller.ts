4ea9df31db5ed08b5375e3409c57b3aefcc73cd3
t0nxx <mahmoudko1500@hotmail.com>
2021-06-13 19:14:50 +0200

    add auth middleware to lookups

--- src/lookups/lookups.controller.ts
+++ src/lookups/lookups.controller.ts
@@ -1,3 +1,4 @@
+import { User } from '@/user/user.decorator';
 import { Controller, Get, Headers, Param } from '@nestjs/common';
 import {
   ApiBearerAuth,
@@ -23,7 +24,7 @@ export class LookupsController {
   @Get('v1/lookups/listing-types/:type')
   async getListingTypes(
     @Param('type') type: string,
-    @Headers('lang') lang = 'en',
+    @User('lang') lang: string,
   ): Promise<ListingTypeDto[]> {
     return await this.lookupsService.getListingTypes(type, lang);
   }
@@ -31,28 +32,28 @@ export class LookupsController {
   @ApiOperation({ summary: 'Gets price types' })
   @ApiResponse({ status: 200, description: 'Returns the price types' })
   @Get('v1/lookups/price-types')
-  async getPriceTypes(@Headers('lang') lang = 'en'): Promise<PriceTypeDto[]> {
+  async getPriceTypes(@User('lang') lang: string): Promise<PriceTypeDto[]> {
     return await this.lookupsService.getPriceTypes(lang);
   }
 
   @ApiOperation({ summary: 'Gets currencies' })
   @ApiResponse({ status: 200, description: 'Returns the currency list' })
   @Get('v1/lookups/currencies')
-  async getCurrencies(@Headers('lang') lang = 'en'): Promise<CurrencyDto[]> {
+  async getCurrencies(@User('lang') lang: string): Promise<CurrencyDto[]> {
     return await this.lookupsService.getCurrencies(lang);
   }
 
   @ApiOperation({ summary: 'Gets hiring types' })
   @ApiResponse({ status: 200, description: 'Returns the hiring types' })
   @Get('v1/lookups/hiring-types')
-  async getHiringTypes(@Headers('lang') lang = 'en'): Promise<HiringTypeDto[]> {
+  async getHiringTypes(@User('lang') lang: string): Promise<HiringTypeDto[]> {
     return await this.lookupsService.getHiringTypes(lang);
   }
 
   @ApiOperation({ summary: 'Gets Disclaimer options' })
   @ApiResponse({ status: 200, description: 'Returns the disclaimer options' })
   @Get('v1/lookups/disclaimers')
-  async getDisclaimers(@Headers('lang') lang = 'en'): Promise<DisclaimerDto[]> {
+  async getDisclaimers(@User('lang') lang: string): Promise<DisclaimerDto[]> {
     return await this.lookupsService.getDisclaimers(lang);
   }
 }
