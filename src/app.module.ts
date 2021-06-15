6e626b0c2c39ac0cd7672e2e0bc137402630c955
t0nxx <mahmoudko1500@hotmail.com>
2021-06-15 03:11:06 +0200

    add sentry logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,27 @@ import { DevModule } from './dev/dev.module';
 import { LookupsModule } from './lookups/lookups.module';
 import { AppLoggerModule } from './shared/appLogger/appLogger.module';
 import { StoryModule } from './story/story.module';
+import { AllExceptionsFilter } from './shared/exception-filters/all-exception-filter-responser';
+import { APP_FILTER } from '@nestjs/core';
+import { SentryModule } from '@ntegral/nestjs-sentry';
+import { LogLevel } from '@sentry/types';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal : true,
+      isGlobal: true,
       envFilePath:
         process.env.NODE_ENV == 'production' ? '.env.prod' : '.env.dev',
     }),
+    // sentry module
+    SentryModule.forRoot({
+      dsn: 'https://8a07ccff5de9497f923800ed5ef9d435@o412866.ingest.sentry.io/5816558',
+      debug: true,
+      environment: 'production',
+      release: null,
+      logLevel: LogLevel.Error,
+    }),
+
     UserModule,
     ListingModule,
     DevModule,
@@ -21,7 +35,12 @@ import { StoryModule } from './story/story.module';
     StoryModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter,
+    },
+  ],
 })
 export class ApplicationModule {
   constructor() {}
---
+++ src/shared/exception-filters/all-exception-filter-responser.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { InjectSentry, SentryService } from '@ntegral/nestjs-sentry';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  constructor(@InjectSentry() private readonly sentryClient: SentryService) {}
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    // here to log all execptions / errors
+    const errObject = {
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      method: request.method,
+      error:
+        JSON.stringify(exception.getResponse()) ||
+        'No error message set for this error',
+      stackTrace: exception.stack,
+      body: JSON.stringify(request.body) || null,
+      headers: request.headers || null,
+      cookies: request.cookies || null,
+      clientIp: request.ip || null,
+    };
+
+    this.sentryClient.instance().captureException(errObject);
+    response.status(status).json({
+      success: false,
+      /// this is cheating method to flat , since spread op is only for objects
+      ...(exception.getResponse() as {}),
+    });
+  }
+}
