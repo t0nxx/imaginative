3effcf28e2cb811effc20510c8735b1c4886ee37
t0nxx <mahmoudko1500@hotmail.com>
2021-10-13 19:24:43 +0200

    test new build

--- src/story/dto/CreateStory.dto.ts
+++ src/story/dto/CreateStory.dto.ts
@@ -24,16 +24,16 @@ export default class CreateStoryDto {
   @IsOptional() otherImaginativeYear: string;
 
   /// optional if user want to upload image with each section
-  /// cause the front end send null not [] in case no items , i should map the props to empty array in case the request body is null 
+  /// cause the front end send null not [] in case no items , i should map the props to empty array in case the request body is null
   @IsOptional()
   @Transform(({ value }) => (value ? value : []))
-  introImages: string[];
+  introImages: string[] = [];
   @IsOptional()
   @Transform(({ value }) => (value ? value : []))
-  bodyImages: string[];
+  bodyImages: string[] = [];
   @IsOptional()
   @Transform(({ value }) => (value ? value : []))
-  conclusionImages: string[];
+  conclusionImages: string[] = [];
 
   /// this is optional since it will be sent only if story of type product based on (disclaimerId) is promote product
   @IsOptional() tagline: string;
